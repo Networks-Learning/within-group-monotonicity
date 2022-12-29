ded9e26fe50cb2fc9ff6f1cedbe870a41d387882
Nastaran <nastaran.okati@gmail.com>
2022-12-29 11:22:56 +0200

    upd run

--- scripts/plot_exp_wgm.py
+++ scripts/plot_exp_wgm.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
         axs[2][0].set_ylabel(r'$|\mathcal{B}|$')
         axs[3][0].set_ylabel(r'Shortlist Size')
         axs[2][0].yaxis.set_major_locator(ticker.MultipleLocator(2))
-        axs[3][0].set_ylim(top=7)
+        axs[3][0].set_ylim(top=8)
         axs[3][0].yaxis.set_major_locator(ticker.MultipleLocator(1))
 
         algorithms = []
