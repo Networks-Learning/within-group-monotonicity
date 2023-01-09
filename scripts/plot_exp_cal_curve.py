c97e5f4cc3790f4b4fa0fcec552337727d6d2a4c
Nastaran <nastaran.okati@gmail.com>
2023-01-09 12:08:10 +0200

    upd run

--- scripts/plot_exp_cal_curve.py
+++ scripts/plot_exp_cal_curve.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
                 fig.legend(handles=handles,loc='upper center', bbox_to_anchor=(0.52, 1.02), ncol=4)
     plt.figtext(x=0.21, y=0.82, s=Z_labels[Z[0][0]]["feature"], fontsize=font_size)
     plt.figtext(x=0.73, y=0.82, s=Z_labels[Z[1][0]]["feature"], fontsize=font_size)
-    axs[0].set_ylabel(metric_labels[metrics[0]])
+    # axs[0].set_ylabel(metric_labels[metrics[0]])
 
     plt.tight_layout(rect=[0, 0, 1, 0.82])
     fig.savefig("./plots/exp_cal_curve.pdf", format="pdf")
