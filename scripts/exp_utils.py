563fdcfed9a33dab98f508764f6fb32daf4f26cc
Nastaran <nastaran.okati@gmail.com>
2023-01-13 14:18:11 +0200

    upd run

--- scripts/exp_utils.py
+++ scripts/exp_utils.py
@@ -102,7 +102,7 @@ def generate_commands(exp_dir, Z, n_trains, n_cals, n_test, lbds, runs, n_runs_t
                                               " --wgm_path {} --result_path {} --k {} --m {}  --B {} " \
                                               "--scaler_path {} --n_runs_test {}".format("_".join([str(index) for index in Z_indices]),cal_data_path, test_raw_path, classifier_path, wgm_path,
                                                                         wgm_result_path, k, n_test,umb_num_bin, scaler_path,n_runs_test)
-                                # exp_commands.append(wgm_command)
+                                exp_commands.append(wgm_command)
                                 # print("training wgm starting from umb with {} bins".format(umb_num_bin))
                                 # if os.system(wgm_command)==256:
                                 #     return
@@ -117,7 +117,7 @@ def generate_commands(exp_dir, Z, n_trains, n_cals, n_test, lbds, runs, n_runs_t
                                     "_".join([str(index) for index in Z_indices]), cal_data_path, test_raw_path,
                                     classifier_path, wgc_path,
                                     wgc_result_path, k, n_test, umb_num_bin, scaler_path, n_runs_test)
-                                # exp_commands.append(wgc_command)
+                                exp_commands.append(wgc_command)
 
                                 pav_path = os.path.join(exp_dir, exp_identity_string + "_pav.pkl")
                                 pav_result_path = os.path.join(exp_dir,
@@ -130,91 +130,60 @@ def generate_commands(exp_dir, Z, n_trains, n_cals, n_test, lbds, runs, n_runs_t
                                     classifier_path, pav_path,
                                     pav_result_path, k, n_test, umb_num_bin, scaler_path,
                                     n_runs_test)
-                                # exp_commands.append(pav_command)
+                                exp_commands.append(pav_command)
 
                             commands.append(exp_commands)
     return commands
 
 
-def generate_commands_diversity(exp_dir, n_train, n_trains_min, n_cal_maj, n_cals_min, n_test, n_test_maj,
-                                n_test_min, lbds, runs, n_runs_test, k_maj, k_min, alpha, classifier_type,
-                                umb_num_bins, train_cal_maj_raw_path, train_cal_min_raw_path, test_raw_path,
-                                noise_ratio_maj=0, noise_ratios_min=[-1]):
+
+def generate_commands_discrimination(exp_dir, Z, n_trains, n_cals, n_test, lbds, runs, n_runs_test, k, classifier_type,
+                      umb_num_bins, train_cal_raw_path, test_raw_path, noise_ratios=[-1]):
     """
-    generate a list of commands from the diversity experiment setup
+    generate a list of commands from the experiment setup
     """
     commands = []
-    for n_train_min in n_trains_min:
-        n_train_maj = n_train - n_train_min
-        for noise_ratio_min in noise_ratios_min:
-            for n_cal_min in n_cals_min:
-                for lbd in lbds:
-                    for run in runs:
-                        exp_identity_string = "_".join([str(n_train_min), str(noise_ratio_min), str(n_cal_min), lbd, str(run)])
-                        train_data_path = os.path.join(exp_dir, exp_identity_string + "_train_data.pkl")
-                        cal_data_maj_path = os.path.join(exp_dir, exp_identity_string + "_cal_data_maj.pkl")
-                        cal_data_min_path = os.path.join(exp_dir, exp_identity_string + "_cal_data_min.pkl")
-                        scaler_path = os.path.join(exp_dir, exp_identity_string + "_scaler.pkl")
-                        data_generation_command = "python ./scripts/generate_data_diversity.py --n_train_maj {} " \
-                                                  "--n_train_min {} --n_cal_maj {} --n_cal_min {} " \
-                                                  "--train_cal_maj_raw_path {} --train_cal_min_raw_path {} " \
-                                                  "--train_data_path {} --cal_data_maj_path {} --cal_data_min_path {} " \
-                                                  "--scaler_path {}".format(n_train_maj, n_train_min, n_cal_maj, n_cal_min,
-                                                                            train_cal_maj_raw_path, train_cal_min_raw_path,
-                                                                            train_data_path, cal_data_maj_path,
-                                                                            cal_data_min_path, scaler_path)
-                        classifier_path = os.path.join(exp_dir, exp_identity_string + "_classifier.pkl")
-                        if classifier_type == "LR":
-                            train_classifier_command = "python ./src/train_LR.py --train_data_path {} --lbd {} " \
-                                                       "--noise_ratio_maj {} --noise_ratio_min {} " \
-                                                       "--classifier_path {}".format(train_data_path, lbd,
-                                                                                     noise_ratio_maj, noise_ratio_min,
-                                                                                     classifier_path)
-                        elif classifier_type == "MLP":
-                            train_classifier_command = "python ./src/train_MLP.py --train_data_path {} --lbd {} " \
-                                                       "--classifier_path {}".format(train_data_path, lbd, classifier_path)
-                        elif classifier_type == "NB":
-                            train_classifier_command = "python ./src/train_NB.py --train_data_path {} " \
-                                                       "--classifier_path {}".format(train_data_path, classifier_path)
-                        else:
-                            raise ValueError("Classifier {} not supported".format(classifier_type))
-                        css_result_path = os.path.join(exp_dir, exp_identity_string + "_css_result.pkl")
-                        css_command = "python ./src/css_diversity.py --cal_data_maj_path {} --cal_data_min_path {} " \
-                                      "--test_raw_path {} --classifier_path {} --result_path {} --k_maj {} --k_min {} " \
-                                      "--m_maj {} --m_min {} --alpha {} --scaler_path " \
-                                      "{}".format(cal_data_maj_path, cal_data_min_path, test_raw_path, classifier_path,
-                                                  css_result_path, k_maj, k_min, n_test_maj, n_test_min, alpha, scaler_path)
-                        css_naive_result_path = os.path.join(exp_dir, exp_identity_string + "_css_naive_result.pkl")
-                        css_naive_command = "python ./src/css_diversity_naive.py --cal_data_maj_path {} " \
-                                            "--cal_data_min_path {} --test_raw_path {} --classifier_path {} " \
-                                            "--result_path {} --k_maj {} --k_min {} --m_maj {} --m_min {} --alpha {} " \
-                                            "--scaler_path {}".format(cal_data_maj_path, cal_data_min_path, test_raw_path,
-                                                                      classifier_path, css_naive_result_path, k_maj, k_min,
-                                                                      n_test_maj, n_test_min, alpha, scaler_path)
-                        ucss_result_path = os.path.join(exp_dir, exp_identity_string + "_ucss_result.pkl")
-                        ucss_command = "python ./src/ucss_diversity.py --test_raw_path {} " \
-                                       "--classifier_path {} --result_path {} --k_maj {} --k_min {} --m {} " \
-                                       "--n_runs_test {} --scaler_path {}".format(test_raw_path, classifier_path,
-                                                                                  ucss_result_path, k_maj, k_min, n_test,
-                                                                                  n_runs_test, scaler_path)
-                        iso_reg_ss_result_path = os.path.join(exp_dir, exp_identity_string + "_iso_reg_ss_result.pkl")
-                        iso_reg_ss_command = "python ./src/iso_reg_ss_diversity.py --cal_data_maj_path {} " \
-                                             "--cal_data_min_path {} --test_raw_path {} --classifier_path {} " \
-                                             "--result_path {} --k_maj {} --k_min {} --m_maj {} --m_min {} " \
-                                             "--scaler_path {}".format(cal_data_maj_path, cal_data_min_path, test_raw_path,
-                                                                       classifier_path, iso_reg_ss_result_path,  k_maj,
-                                                                       k_min, n_test_maj, n_test_min, scaler_path)
-                        platt_scal_ss_result_path = os.path.join(exp_dir, exp_identity_string + "_platt_scal_ss_result.pkl")
-                        platt_scal_ss_command = "python ./src/platt_scal_ss_diversity.py --cal_data_maj_path {} " \
-                                               "--cal_data_min_path {} --test_raw_path {} --classifier_path {} " \
-                                               "--result_path {} --k_maj {} --k_min {} --m {} --n_runs_test {} " \
-                                               "--scaler_path {}".format(cal_data_maj_path, cal_data_min_path,
-                                                                         test_raw_path, classifier_path,
-                                                                         platt_scal_ss_result_path, k_maj, k_min, n_test,
-                                                                         n_runs_test, scaler_path)
-                        exp_commands = [data_generation_command, train_classifier_command, css_command, css_naive_command,
-                                        ucss_command, iso_reg_ss_command, platt_scal_ss_command]
-                        commands.append(exp_commands)
+    for Z_indices in Z:
+        for n_train in n_trains:
+            for noise_ratio in noise_ratios:
+                for n_cal in n_cals:
+                    for lbd in lbds:
+                        for run in runs:
+                            exp_identity_string = "_".join(["_".join([str(index) for index in Z_indices]), str(n_train), str(noise_ratio), str(n_cal), lbd, str(run)])
+                            print("adding Experiment: " + exp_identity_string)
+                            train_data_path = os.path.join(exp_dir, exp_identity_string + "_train_data.pkl")
+                            cal_data_path = os.path.join(exp_dir, exp_identity_string + "_cal_data.pkl")
+                            scaler_path = os.path.join(exp_dir, exp_identity_string + "_scaler.pkl")
+                            data_generation_command = "python ./scripts/generate_data.py --n_train {} --n_cal {} " \
+                                                      "--train_cal_raw_path {} --train_data_path {} --cal_data_path {} " \
+                                                      "--scaler_path {}".format(n_train, n_cal, train_cal_raw_path,
+                                                                             train_data_path, cal_data_path, scaler_path)
+
+                            classifier_path = os.path.join(exp_dir, exp_identity_string + "_classifier.pkl")
+                            if classifier_type == "LR":
+                                train_classifier_command = "python ./src/train_LR.py --Z_indices {} --train_data_path {} --cal_data_path {} --lbd {} " \
+                                                           "--noise_ratio_maj {} --noise_ratio_min {} " \
+                                                           "--classifier_path {}".format("_".join([str(index) for index in Z_indices]), train_data_path, cal_data_path, lbd, noise_ratio,
+                                                                                         noise_ratio, classifier_path)
+                            else:
+                                raise ValueError("Classifier {} not supported".format(classifier_type))
+
+
+                            exp_commands = [data_generation_command, train_classifier_command]
+                            # exp_commands = []
+                            for umb_num_bin in umb_num_bins:
+                                umb_result_path = os.path.join(exp_dir, exp_identity_string +
+                                                               "_umb_{}_result.pkl".format(umb_num_bin))
+                                umb_path = os.path.join(exp_dir, exp_identity_string + "_umb.pkl")
+                                umb_prediction_command = "python ./src/umb_ss.py --Z_indices {} --cal_data_path {} " \
+                                                         "--test_raw_path {} --classifier_path {} --umb_path {} --result_path {} --k {}" \
+                                                         " --m {} --B {} " \
+                                                         "--scaler_path {} --n_runs_test {}".format("_".join([str(index) for index in Z_indices]), cal_data_path, test_raw_path,
+                                                                                   classifier_path, umb_path, umb_result_path, k, n_test,
+                                                                                    umb_num_bin, scaler_path,n_runs_test)
+                                exp_commands.append(umb_prediction_command)
+
+                            commands.append(exp_commands)
     return commands
 
 
---
+++ scripts/params_exp_discrimination.py
@@ -0,0 +1,38 @@
+exp_token = "cz"
+exp_dir = "./exp_discrimination"
+q_ratio = "0.2"
+test_ratio = "0.5"
+prepare_data = False
+submit = True
+split_size = 1000
+n_test = 100
+k = 5
+# Z = [[2],[4],[10],[6],[14]]   #valid ones 2,4,6,10,14
+Z = [[6],[15],[14],[0],[4]]#,[15]]#,[0],[14]]#,[1],[2],[4],[6],[14],[15]]
+# Z = [[14],[15]]
+# Z = [[2],[6],[10],[14]]   #valid ones
+n_runs = 100
+n_runs_test = 100#000
+n_train = 100000
+n_trains = [100000]
+noise_ratio = -1
+noise_ratios = [noise_ratio]
+n_cals = [50000]#[10000, 50000, 100000, 500000,1000000]#, 2000, 5000, 10000, 20000, 50000, 100000]
+n_cals_label = ["5e4"]#, "5e4", "1e5", "5e5", "1e6"]
+runs = list(range(n_runs))
+classifier_type = "LR"
+lbd = "1e-6"
+lbds = ["1e-6"]
+umb_num_bins = [45,55,65,75]
+
+ks = [5,10,15]
+
+
+
+#0: age
+#1: school
+#2: marital status
+#4: disability
+#6: citizenship
+#14: sex
+#15: race
