0dfd4df4bc9001997f5f37cbbd9f6bf41b8cce25
Nastaran <nastaran.okati@gmail.com>
2023-01-15 12:34:19 +0200

    upd run

--- scripts/plot_group_discrimination.py
+++ scripts/plot_group_discrimination.py
@@ -11,7 +11,7 @@ plt.rc('font', family='serif')
 
 
 if __name__ == "__main__":
-    from params_exp_violations import *
+    from params_exp_bins import *
     from matplotlib.ticker import StrMethodFormatter
 
     algorithm_labels = {}
